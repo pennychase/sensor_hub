9a48eae5f31d545b3e2a30d4bb1105d24790643c
Penny Chase <mpchase@mac.com>
2025-01-17 18:49:04 -0500

    Updated dependencies

--- sensor_hub/config/config.exs
+++ sensor_hub/config/config.exs
@@ -16,7 +16,7 @@ config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 # Set the SOURCE_DATE_EPOCH date for reproducible builds.
 # See https://reproducible-builds.org/docs/source-date-epoch/ for more information
 
-config :nerves, source_date_epoch: "1734826489"
+config :nerves, source_date_epoch: "1737124941"
 
 if Mix.target() == :host do
   import_config "host.exs"
--- sensor_hub/mix.exs
+++ sensor_hub/mix.exs
@@ -47,8 +47,7 @@ defmodule SensorHub.MixProject do
       {:shoehorn, "~> 0.9.1"},
       {:ring_logger, "~> 0.11.0"},
       {:toolshed, "~> 0.4.0"},
-      {:nerves_motd, "~> 0.1.0"},
-      {:circuits_i2c, "~> 2.0.6"},
+      {:circuits_i2c, "~> 2.0"},
 
       # Allow Nerves.Runtime on host to support development, testing and CI.
       # See config/host.exs for usage.
@@ -56,7 +55,7 @@ defmodule SensorHub.MixProject do
 
       # Dependencies for all targets except :host
       {:nerves_pack, "~> 0.7.1", targets: @all_targets},
-      
+
       # Dependencies for specific targets
       # NOTE: It's generally low risk and recommended to follow minor version
       # bumps to Nerves systems. Since these include Linux kernel and Erlang
